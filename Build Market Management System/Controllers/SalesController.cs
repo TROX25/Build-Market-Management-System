be5533f1f8b5666c57e2d61ab32a3950ebe117ee
TROX25 <pimalogik@gmail.com>
2025-12-29 21:50:26 +0100

    Preparing to add Cashier's (sell) console

---
+++ Build Market Management System/Controllers/SalesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Build_Market_Management_System.Controllers
+{
+    public class SalesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
