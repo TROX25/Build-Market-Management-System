249a5a9b6ed3c37d605bbed2120776751478db41
TROX25 <pimalogik@gmail.com>
2026-01-04 21:05:13 +0100

    Added ViewComponent for Transaction

---
+++ Build Market Management System/Models/ViewComponents/TransactionsViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Build_Market_Management_System.Models.ViewComponents
+{
+    [ViewComponent]
+    public class TransactionsViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(string userName)
+        {
+            var transactions = TransactionsRepository.GetByDayAndCashier(userName, DateTime.Now);
+            return View(transactions);
+        }
+
+         
+    }
+}
